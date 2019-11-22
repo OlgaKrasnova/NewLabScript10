2bf1a4da8927979f79344f7c0569388c411589d1
Olga <krasolga2018@gmail.com>
2019-11-22 10:00:36 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Sensor } from './shared/models/sensor.model';
+import { TouchSequence } from 'selenium-webdriver';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  sensors: Sensor[] = [];
+  constructor() {
+    for(let i=0; i < 10; i++) {
+      this.sensors.push(new Sensor(i, `Датчик_${i}`));  
+    }
+  }
+
+  onDeleteSensor(idx: number) {
+    this.sensors.splice(idx, 1);
+  }
+
+  onCreateSensor(new_name, new_status) {
+    if(this.sensors.length == 0) {
+      this.sensors.push(new Sensor(1, new_name.value, new_status.checked));
+    }
+    else
+      this.sensors.push(new Sensor(this.sensors[this.sensors.length - 1].id + 1, new_name.value, new_status.checked));
+  }
+}
